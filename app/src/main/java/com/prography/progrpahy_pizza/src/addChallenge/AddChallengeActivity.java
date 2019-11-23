a8d9a13c01b67a0af59d0ba0aa25ff57f05b56a0
조연우 <wooisso@naver.com>
2019-11-23 17:43:49 +0900

    #13-3 FIX Json Issue

--- app/src/main/java/com/prography/progrpahy_pizza/src/addChallenge/AddChallengeActivity.java
+++ app/src/main/java/com/prography/progrpahy_pizza/src/addChallenge/AddChallengeActivity.java
@@ -3,62 +3,37 @@ package com.prography.progrpahy_pizza.src.addChallenge;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
 
 import com.prography.progrpahy_pizza.R;
 import com.prography.progrpahy_pizza.src.BaseActivity;
 import com.prography.progrpahy_pizza.src.addChallenge.interfaces.AddChallengeActivityView;
 
-import androidx.annotation.Nullable;
-
 public class AddChallengeActivity extends BaseActivity implements AddChallengeActivityView {
+
     private Spinner spnRoutineType;
     private EditText edtTime;
     private Spinner spnObjectUnit;
     private Spinner spnExerciseType;
     private Button btn_addChallenge;
 
-    private String routineType;
-    private String time;
-    private String objectUnit;
-    private String exerciseType;
-
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addchallenge);
 
-        spnRoutineType = findViewById(R.id.spinner_routineType);
         edtTime = findViewById(R.id.time);
+        spnRoutineType = findViewById(R.id.spinner_routineType);
         spnObjectUnit = findViewById(R.id.spinner_objectUnit);
         spnExerciseType = findViewById(R.id.spinner_exerciseType);
         btn_addChallenge = findViewById(R.id.btn_challengeAssign);
 
 
-        spnRoutineType.setOnItemSelectedListener(this);
-        spnObjectUnit.setOnItemSelectedListener(this);
-        spnExerciseType.setOnItemSelectedListener(this);
-
-
-        btn_addChallenge.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.btn_challengeAssign:
-                        time = edtTime.getText().toString();
-                        double t = Double.parseDouble(time);
-                        tryPostChallenge(routineType, t, objectUnit, exerciseType);
-                        //서버로 post 하고 postvalidateSuccess 하면, mainActivity에 intent 넘겨주고 finish();
-
-
-                    default:
-                        setResult(RESULT_CANCELED);
-                }
-
-            }
-        });
+        /* Set On Click Listener */
+        btn_addChallenge.setOnClickListener(this);
 
     }
 
@@ -69,10 +44,10 @@ public class AddChallengeActivity extends BaseActivity implements AddChallengeAc
 
         //for recyclerView
         Intent intent = new Intent();
-        intent.putExtra("routineType", routineType);
-        intent.putExtra("time", time);
-        intent.putExtra("objectUnit", objectUnit);
-        intent.putExtra("exerciseType", exerciseType);
+        intent.putExtra("routineType", spnRoutineType.getSelectedItem().toString());
+        intent.putExtra("time", Double.parseDouble(edtTime.getText().toString()));
+        intent.putExtra("objectUnit", spnObjectUnit.getSelectedItem().toString());
+        intent.putExtra("exerciseType", spnExerciseType.getSelectedItem().toString());
 
         setResult(RESULT_OK, intent);
         finish();
@@ -92,22 +67,14 @@ public class AddChallengeActivity extends BaseActivity implements AddChallengeAc
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch (view.getId()) {
-            case R.id.spinner_exerciseType:
-                exerciseType = (String) spnExerciseType.getItemAtPosition(position);
-                break;
-            case R.id.spinner_objectUnit:
-                objectUnit = (String) spnObjectUnit.getItemAtPosition(position);
-                break;
-            case R.id.spinner_routineType:
-                routineType = (String) spnRoutineType.getItemAtPosition(position);
-                break;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_challengeAssign:
+                tryPostChallenge(spnRoutineType.getSelectedItem().toString(),
+                        Double.parseDouble(edtTime.getText().toString()),
+                        spnObjectUnit.getSelectedItem().toString(),
+                        spnExerciseType.getSelectedItem().toString());
+                //서버로 post 하고 postvalidateSuccess 하면, mainActivity에 intent 넘겨주고 finish();
         }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
--- app/src/main/java/com/prography/progrpahy_pizza/src/addChallenge/interfaces/AddChallengeActivityView.java
+++ app/src/main/java/com/prography/progrpahy_pizza/src/addChallenge/interfaces/AddChallengeActivityView.java
@@ -1,8 +1,9 @@
 package com.prography.progrpahy_pizza.src.addChallenge.interfaces;
 
+import android.view.View;
 import android.widget.AdapterView;
 
-public interface AddChallengeActivityView extends AdapterView.OnItemSelectedListener{
+public interface AddChallengeActivityView extends View.OnClickListener{
     void postvalidateSuccess();
     void postvalidateFailure();
 }
