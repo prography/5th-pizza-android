ade28e685cfe442202f39558644142b992ed505e
조연우 <wooisso@naver.com>
2019-11-23 21:43:55 +0900

    #16. ADJUST Layouts FOR addchallenge
    issue. pickerview custom

--- app/src/main/java/com/prography/progrpahy_pizza/src/addChallenge/AddChallengeActivity.java
+++ app/src/main/java/com/prography/progrpahy_pizza/src/addChallenge/AddChallengeActivity.java
@@ -2,24 +2,29 @@ package com.prography.progrpahy_pizza.src.addChallenge;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.prography.progrpahy_pizza.R;
 import com.prography.progrpahy_pizza.src.BaseActivity;
+import com.prography.progrpahy_pizza.src.addChallenge.fragments.SelectorBottomSheetFragment;
 import com.prography.progrpahy_pizza.src.addChallenge.interfaces.AddChallengeActivityView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 
 public class AddChallengeActivity extends BaseActivity implements AddChallengeActivityView {
 
-    private Spinner spnRoutineType;
-    private EditText edtTime;
-    private Spinner spnObjectUnit;
-    private Spinner spnExerciseType;
-    private Button btn_addChallenge;
+    private TextView tvDate;
+    private TextView tvTimeOrDistance;
+    private TextView tvType;
+    private Button btnSubmit;
+    private Toolbar tbAddChallenge;
 
     private String routine_type;
     private double quota;
@@ -30,16 +35,36 @@ public class AddChallengeActivity extends BaseActivity implements AddChallengeAc
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addchallenge);
 
-        edtTime = findViewById(R.id.time);
-        spnRoutineType = findViewById(R.id.spinner_routineType);
-        spnObjectUnit = findViewById(R.id.spinner_objectUnit);
-        spnExerciseType = findViewById(R.id.spinner_exerciseType);
-        btn_addChallenge = findViewById(R.id.btn_challengeAssign);
+        /* findViewByID */
+        tvDate = findViewById(R.id.tv_date_addchallenge);
+        tvTimeOrDistance = findViewById(R.id.tv_time_addchallenge);
+        tvType = findViewById(R.id.tv_type_addchallenge);
+        btnSubmit = findViewById(R.id.btn_challengeAssign);
+        tbAddChallenge = findViewById(R.id.toolbar_addchallenge);
 
+        /* Toolbar */
+        setSupportActionBar(tbAddChallenge);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_close);
 
         /* Set On Click Listener */
-        btn_addChallenge.setOnClickListener(this);
+        btnSubmit.setOnClickListener(this);
+        tvType.setOnClickListener(this);
+        tvDate.setOnClickListener(this);
+        tvTimeOrDistance.setOnClickListener(this);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
     }
 
     @Override
@@ -49,10 +74,10 @@ public class AddChallengeActivity extends BaseActivity implements AddChallengeAc
 
         //for recyclerView
         Intent intent = new Intent();
-        intent.putExtra("routineType", spnRoutineType.getSelectedItem().toString());
-        intent.putExtra("time", Double.parseDouble(edtTime.getText().toString()));
-        intent.putExtra("objectUnit", spnObjectUnit.getSelectedItem().toString());
-        intent.putExtra("exerciseType", spnExerciseType.getSelectedItem().toString());
+        //intent.putExtra("routineType", spnRoutineType.getSelectedItem().toString());
+        //intent.putExtra("time", Double.parseDouble(edtTime.getText().toString()));
+        //intent.putExtra("objectUnit", spnObjectUnit.getSelectedItem().toString());
+        //intent.putExtra("exerciseType", spnExerciseType.getSelectedItem().toString());
 
         setResult(RESULT_OK, intent);
         finish();
@@ -107,11 +132,24 @@ public class AddChallengeActivity extends BaseActivity implements AddChallengeAc
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_challengeAssign:
-                tryPostChallenge(spnRoutineType.getSelectedItem().toString(),
-                        Double.parseDouble(edtTime.getText().toString()),
+                /*tryPostChallenge(spnRoutineType.getSelectedItem().toString(),
+                Double.parseDouble(edtTime.getText().toString()),
                         spnObjectUnit.getSelectedItem().toString(),
-                        spnExerciseType.getSelectedItem().toString());
+                        spnExerciseType.getSelectedItem().toString());*/
                 //서버로 post 하고 postvalidateSuccess 하면, mainActivity에 intent 넘겨주고 finish();
+                break;
+            case R.id.tv_date_addchallenge:
+                SelectorBottomSheetFragment selectorBottomSheetFragment = new SelectorBottomSheetFragment();
+                selectorBottomSheetFragment.show(getSupportFragmentManager(), "selector");
+                break;
+            case R.id.tv_type_addchallenge:
+                SelectorBottomSheetFragment selectorBottomSheetFragment2 = new SelectorBottomSheetFragment();
+                selectorBottomSheetFragment2.show(getSupportFragmentManager(), "selector");
+                break;
+            case R.id.tv_time_addchallenge:
+                SelectorBottomSheetFragment selectorBottomSheetFragment3 = new SelectorBottomSheetFragment();
+                selectorBottomSheetFragment3.show(getSupportFragmentManager(), "selector");
+                break;
         }
     }
 }
---
+++ app/src/main/java/com/prography/progrpahy_pizza/src/addChallenge/fragments/SelectorBottomSheetFragment.java
@@ -0,0 +1,22 @@
+package com.prography.progrpahy_pizza.src.addChallenge.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.prography.progrpahy_pizza.R;
+
+public class SelectorBottomSheetFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_selectorbottomsheet, container, false);
+        return view;
+    }
+}
