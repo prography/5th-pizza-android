0a09df1d052d439349f9505e1f50929c7635448b
sunmin Lee <25thbam@gmail.com>
2020-01-04 15:47:07 +0900

    #61-1. ADD skipButton on tutorial

--- app/src/main/java/com/prography/prography_pizza/src/tutorial/TutorialActivity.java
+++ app/src/main/java/com/prography/prography_pizza/src/tutorial/TutorialActivity.java
@@ -1,21 +1,26 @@
 package com.prography.prography_pizza.src.tutorial;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.prography.prography_pizza.R;
 import com.prography.prography_pizza.src.BaseActivity;
+import com.prography.prography_pizza.src.splash.SplashActivity;
 import com.prography.prography_pizza.src.tutorial.adapter.TutorialPager;
+import com.prography.prography_pizza.src.tutorial.interfaces.TutorialView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import me.relex.circleindicator.CircleIndicator;
 
-public class TutorialActivity extends BaseActivity {
+public class TutorialActivity extends BaseActivity implements TutorialView {
     private FragmentPagerAdapter fragmentPagerAdapter;
 
-    private ViewPager viewPager;
-    private CircleIndicator circleIndicator;
+    private ViewPager vpTutorial;
+    private CircleIndicator cindicator;
+    private TextView tvSkip;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,12 +28,22 @@ public class TutorialActivity extends BaseActivity {
         setContentView(R.layout.activity_tutorial);
 
         /* findViewByID */
-        viewPager=findViewById(R.id.vp_tutorial);
-        circleIndicator=findViewById(R.id.ci_tutorial);
+        vpTutorial =findViewById(R.id.vp_tutorial);
+        cindicator =findViewById(R.id.ci_tutorial);
+        tvSkip=findViewById(R.id.tv_skip_tutorial);
 
         /* Init View */
         fragmentPagerAdapter=new TutorialPager(getSupportFragmentManager());
-        viewPager.setAdapter(fragmentPagerAdapter);
-        circleIndicator.setViewPager(viewPager);
+        vpTutorial.setAdapter(fragmentPagerAdapter);
+        cindicator.setViewPager(vpTutorial);
+
+        /* Set On Click Listener */
+        tvSkip.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startNextActivity(SplashActivity.class);
+        finish();
     }
 }
---
+++ app/src/main/java/com/prography/prography_pizza/src/tutorial/interfaces/TutorialView.java
@@ -0,0 +1,6 @@
+package com.prography.prography_pizza.src.tutorial.interfaces;
+
+import android.view.View;
+
+public interface TutorialView extends View.OnClickListener {
+}
